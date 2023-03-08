6bb4a843ed1d7857ca8d63e960e406d74d0b30e1
Jesus Sanchez <soyjesusb@gmail.com>
2023-03-08 01:43:01 -0600

    Save enmienda

---
+++ CartasCredito/Controllers/api/EnmiendasController.cs
@@ -0,0 +1,72 @@
+﻿using CartasCredito.Models;
+using CartasCredito.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CartasCredito.Controllers.api
+{
+	[AllowAnonymous]
+	[EnableCors(origins: "*", headers: "*", methods: "*")]
+	public class EnmiendasController : ApiController
+	{
+		// GET api/<controller>
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<controller>/5
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<controller>
+		public RespuestaFormato Post([FromBody] EnmiendaInsertDTO dtoEnmienda)
+		{
+			var rsp = new RespuestaFormato();
+			var usr = "12cb7342-837e-45d9-892c-6818a38a3816";
+
+			try
+			{
+				var modelo = new Enmienda()
+				{
+					CartaCreditoId = dtoEnmienda.CartaCreditoId,
+					ConsideracionesAdicionales = dtoEnmienda.ConsideracionesAdicionales,
+					DescripcionMercancia = dtoEnmienda.DescripcionMercancia,
+					FechaLimiteEmbarque = dtoEnmienda.FechaLimiteEmbarque,
+					FechaVencimiento = dtoEnmienda.FechaVencimiento,
+					ImporteLC = dtoEnmienda.ImporteLC,
+					InstruccionesEspeciales = dtoEnmienda.InstruccionesEspeciales,
+					CreadoPor = usr
+				};
+
+				rsp = Enmienda.Insert(modelo);
+			}
+			catch (Exception ex)
+			{
+				rsp.Flag = false;
+				rsp.DataString = ex.Message;
+				rsp.Errors.Add(ex.Message);
+			}
+
+			return rsp;
+		}
+
+		// PUT api/<controller>/5
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<controller>/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ CartasCredito/Models/DTOs/EnmiendaInsertDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CartasCredito.Models.DTOs
+{
+	public class EnmiendaInsertDTO
+	{
+		public string ConsideracionesAdicionales {get;set;}
+		public string DescripcionMercancia {get;set;}
+		public DateTime FechaLimiteEmbarque {get;set;}
+		public DateTime FechaVencimiento {get;set;}
+		public decimal ImporteLC {get;set;}
+		public string InstruccionesEspeciales {get;set;}
+		public string CartaCreditoId { get; set; }
+	}
+}
